5690d4025039c137cf3e9187c99da2115f92832b
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-11-03 13:49:39 -0400

    Clean up specs.

--- spec/fake_braintree/customer_spec.rb
+++ spec/fake_braintree/customer_spec.rb
@@ -6,46 +6,50 @@ describe FakeBraintree::SinatraApp do
     let(:expiration_date) { "04/2016" }
     after { FakeBraintree.verify_all_cards = false }
 
-    it "successfully creates a customer" do
-      result = Braintree::Customer.create(
-        :credit_card => {
-          :number          => cc_number,
-          :expiration_date => expiration_date
-        })
+    def create_customer_with_credit_card(options)
+      Braintree::Customer.create(:credit_card => options)
+    end
 
+    it "successfully creates a customer" do
+      result = create_customer_with_credit_card(:number => cc_number,
+                                                :expiration_date => expiration_date)
       result.should be_success
     end
 
-    it "verifies the card number when passed :verify_card => true" do
-      Braintree::Customer.create(
-        :credit_card => {
+    context "when passed :verify_card => true" do
+      it "accepts valid cards" do
+        create_customer_with_credit_card(
           :number          => cc_number,
           :expiration_date => expiration_date,
           :options         => { :verify_card => true }
-        }).should be_success
+        ).should be_success
+      end
 
-      Braintree::Customer.create(
-        :credit_card => {
+      it "rejects invalid cards" do
+        create_customer_with_credit_card(
           :number          => '123456',
           :expiration_date => expiration_date,
           :options         => { :verify_card => true }
-        }).should_not be_success
+        ).should_not be_success
+      end
     end
 
-    it "verifies the card number when FakeBraintree.verify_all_cards == true" do
-      FakeBraintree.verify_all_cards!
+    context "when FakeBraintree.verify_all_cards == true" do
+      before { FakeBraintree.verify_all_cards! }
 
-      Braintree::Customer.create(
-        :credit_card => {
+      it "accepts valid cards" do
+        create_customer_with_credit_card(
           :number          => cc_number,
           :expiration_date => expiration_date
-        }).should be_success
+        ).should be_success
+      end
 
-      Braintree::Customer.create(
-        :credit_card => {
+      it "rejects invalid cards" do
+        create_customer_with_credit_card(
           :number          => '123456',
           :expiration_date => expiration_date
-        }).should_not be_success
+        ).should_not be_success
+      end
     end
   end
 end
