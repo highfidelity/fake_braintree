76675cb76a390d11b00395716af2f66b45fc4c46
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-11-03 13:23:39 -0400

    Reduce duplication.

--- lib/fake_braintree/sinatra_app.rb
+++ lib/fake_braintree/sinatra_app.rb
@@ -7,19 +7,7 @@ module FakeBraintree
     set :raise_errors, true
     disable :logging
 
-    helpers do
-      def gzip(content)
-        ActiveSupport::Gzip.compress(content)
-      end
-
-      def gzipped_response(status_code, uncompressed_content)
-        [status_code, { "Content-Encoding" => "gzip" }, gzip(uncompressed_content)]
-      end
-
-      def md5(content)
-        Digest::MD5.hexdigest(content)
-      end
-    end
+    include Helpers
 
     # Braintree::Customer.create
     post "/merchants/:merchant_id/customers" do
