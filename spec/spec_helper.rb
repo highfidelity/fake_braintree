59a7b600023ee71614d025542922f92247ee11b6
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-01-13 15:14:17 -0500

    Move into a method.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,19 @@
 require 'spork'
 
 Spork.prefork do
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
   require 'rspec'
   require 'fake_braintree'
   require 'timecop'
   require 'bourne'
+
+  def clear_braintree_log
+    Dir.mkdir('tmp') unless File.directory?('tmp')
+    File.new('tmp/braintree_log', 'w').close
+  end
+
   Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 
-  Dir.mkdir('tmp') unless File.directory?('tmp')
-  File.new('tmp/braintree_log', 'w').close
+  clear_braintree_log
 
   TEST_CC_NUMBER = %w(4111 1111 1111 1111).join
 
