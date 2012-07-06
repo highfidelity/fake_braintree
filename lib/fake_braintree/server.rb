9c908391c1a5b89f2860eeca9d6c7a84df2e8aea
Alex Bartlow <bartlowa@gmail.com>
2012-07-06 17:27:27 -0400

    Use a Thin dependency instead of mongrel

--- lib/fake_braintree/server.rb
+++ lib/fake_braintree/server.rb
@@ -1,10 +1,10 @@
 require 'capybara'
 require 'capybara/server'
-require 'rack/handler/mongrel'
+require 'rack/handler/thin'
 
 class FakeBraintree::Server
   def boot
-    with_mongrel_runner do
+    with_thin_runner do
       server = Capybara::Server.new(FakeBraintree::SinatraApp)
       server.boot
       ENV['GATEWAY_PORT'] = server.port.to_s
@@ -13,10 +13,10 @@ class FakeBraintree::Server
 
   private
 
-  def with_mongrel_runner
+  def with_thin_runner
     default_server_process = Capybara.server
     Capybara.server do |app, port|
-      Rack::Handler::Mongrel.run(app, :Port => port)
+      Rack::Handler::Thin.run(app, :Port => port)
     end
     yield
   ensure
