79f850a1b138f1335bf4aaee50ca23228c799db3
Gabe Berke-Williams <gabebw@gabebw.com>
2014-08-08 13:44:08 -0400

    Why was this a fucntion

--- spec/support/braintree_logging.rb
+++ spec/support/braintree_logging.rb
@@ -1,9 +1,7 @@
-def set_up_braintree_logging
-  path = File.expand_path("./tmp/braintree_log")
-  FileUtils.mkdir_p(File.dirname(path))
-  logger = Logger.new(path)
-  logger.level = Logger::DEBUG
-  Braintree::Configuration.logger = logger
-end
+path = File.expand_path("./tmp/braintree_log")
+FileUtils.mkdir_p(File.dirname(path))
 
-set_up_braintree_logging
+logger = Logger.new(path)
+logger.level = Logger::DEBUG
+
+Braintree::Configuration.logger = logger
